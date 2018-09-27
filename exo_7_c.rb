2b6daf01adb94c8b54eb29268b53bd2b78e137e6
beeant <pdelatour7@gmail.com>
2018-09-27 18:59:02 +0200

    Ajout exo_7_c

---
+++ exo_7_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+#La difference entre les trois programmes:
+# 7_a le puts user_name affiche le prenom directement à la ligne sans rien devant
+# 7_b le print ">" rajoute un chevron devant le prenom, le print fait qu'on ne saute pas de ligne ici 
+# 7_c simplement le prenom s'affiche sans rien devant 
