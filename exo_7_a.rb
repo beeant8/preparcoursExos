3618c1ae04a4ca7f03907bde6103f8a9b7e4c1d6
beeant <pdelatour7@gmail.com>
2018-09-27 18:39:37 +0200

    Ajout exo_7_a

---
+++ exo_7_a.rb
@@ -0,0 +1,8 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+# gets.chomp = chomp est une methode string et gets nous retourne qqch. donc gets.chomp nous retourne une string 
+# gets.chomp demande à l'utilisateur de rentrer une variable en l'occurence son nom 
+# puts user_name affiche donc le nom entré par l'utilisateur
+
+
