c3dc8f6bb252ced2b673a37d4e7bf5fd0cf169fb
beeant <pdelatour7@gmail.com>
2018-09-28 17:41:38 +0200

    correction if even?

--- exo_19.rb
+++ exo_19.rb
@@ -1,10 +1,11 @@
-emailNumber=50
+array = Array.new
 total=0
-emailNumber .times do
+
+50 .times do
 	total+=1
-	array=[jean.dupont.0#{total.to_s}@email.fr]
-	arr.at(
+	array = ["jean.dupont.0#{total.to_s}@email.fr"]
+	if total.even? == true
+	puts array
+	
 	end
 end
-
- 
